9659749c0313b4b9f4368872d324682cb46bd7f4
bhargava <whois_1982@yahoo.co.in>
2015-04-28 17:44:46 -0700

    list_print function

--- my_list.c
+++ my_list.c
@@ -1,6 +1,59 @@
 #include "my_list.h"
 
+void list_init(List *list) {
+        list->node_count = 0;
+        list->head = NULL;
+        list->tail = NULL;
+        if(VERBOSE)
+                printf("list_init complete. node_count = %d\n",list->node_count);
+}
+
+void list_print(List *list) {
+        if(list_is_empty(list)) {
+                printf("list is empty! nothing to print\n");
+                return;
+        }
+        Node *current; 
+        printf("LIST_HEAD->");
+        while(current->next != NULL){
+                printf("%d->",current->data);
+                current = current->next;
+        }
+        printf("NULL\n");
+
+}
+
+int insert_next(List *list, Node *node, const void data) {
+        Node *new_node;
+        if((new_node = malloc(sizeof(Node))) == NULL) {
+                printf("malloc failure! exiting..\n");
+                return -1;
+        }
+        if(VERBOSE)
+                printf("malloc successful! data = %d\n",data);
+        return 0;
+
+}
 int main(int argc, char *argv[]) {
+
+        List *list;
+        Node *node;
+        DATA_TYPE *data;
+        
+        data = 5;
+
+        list_init(&list);
+        list_print(&list);
+        
+        int result;
+        result = insert_next(&list, NULL, data);
+        if (result == 0) {
+                list_print(&list);
+        }
+        else {
+                printf("insert_next failed!\n");
+        }
+
 
         return 0;
 }
--- my_list.h
+++ my_list.h
@@ -10,7 +10,25 @@
 #endif
 
 #define VERBOSE TRUE
+#define DATA_TYPE int
 
 #include <stdio.h>
+typedef struct nodeT {
+        void *data;
+        struct nodeT *next; 
+} Node;
+
+typedef struct listT {
+        int node_count;
+        Node *head;
+        Node *tail;
+} List;
+
+void list_init(List *list);
+void list_print(List *list); 
+int insert_next(List *list, Node *node, const void data);
+
+#define list_is_empty(list) (list->node_count == 0? 1:0)
+
 
 #endif
